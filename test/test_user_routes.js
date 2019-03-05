36b91d16e03c39f0b24f2094dbc86c2e5c5cd5ad
kachow6 <kevinchow06@gmail.com>
2019-03-05 15:22:00 -0800

    Testing hidden fails in user route tests

--- test/test_user_routes.js
+++ test/test_user_routes.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 chai.use(chaiHttp);
 
-describe('testing user routes', function() {
+describe('test retrieving users', function() {
 
     before('connect', function() {
         return mongoose.createConnection(dbconfig.url, dbconfig.options, function(err) {
@@ -20,49 +20,10 @@ describe('testing user routes', function() {
     it('should retrieve a list of users', function() {
         chai.request(app)
         .get('/users')
-        .send(function(err, res) {
-            if (err) throw err;
+        .then(function(res) {
+            if (res.error) throw res.error; 
             expect(res).to.have.status(200);
-        });
-    });
-
-    it('should insert new user into db', function() {
-        chai.request(app)
-        .post('/insertuser')
-        .send({ name: 'test', pw: '123' })
-        .then(function(err, res) {
-            if (err) throw err;
-            expect(res).to.have.status(200);
-        }).catch(() => {});
-    });
-
-    it('should retrieve a single user', function() {
-        chai.request(app)
-        .get('/getuser')
-        .query({ name: 'test' })
-        .send(function(err, res) {
-            if (err) throw err;
-            expect(res).to.have.status(200);
-        }).catch(() => {});
-    });
-
-    it('should update user in db', function() {
-        chai.request(app)
-        .post('/updateuser')
-        .send({ name: 'test', pw: '321' })
-        .then(function(err, res) {
-            if (err) throw err;
-            expect(res).to.have.status(200);
-        }).catch(() => {});
-    });
-
-    it('should delete user from db', function() {
-        chai.request(app)
-        .post('/deleteuser')
-        .send({ name: 'test' })
-        .then(function(err, res) {
-            if (err) throw err;
-            expect(res).to.have.status(200);
-        }).catch(() => {});
+            process.exit();
+        }).catch((err) => { console.log(err); });;
     });
 });
